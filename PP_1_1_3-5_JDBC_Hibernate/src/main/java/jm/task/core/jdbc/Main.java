db23e01a26d01082bab0e4feda194281a8b77e39
adrian-0316 <112898743+adrian-0316@users.noreply.github.com>
2023-03-05 23:04:01 +0300

    Add files via upload

---
+++ PP_1_1_3-5_JDBC_Hibernate/src/main/java/jm/task/core/jdbc/Main.java
@@ -0,0 +1,22 @@
+package jm.task.core.jdbc;
+
+import jm.task.core.jdbc.service.UserServiceImpl;
+
+public class Main {
+    public static void main(String[] args) {
+
+/*
+        UserServiceImpl usi = new UserServiceImpl();
+
+        usi.createUsersTable();
+        usi.saveUser("Ivan", "Ivanov", (byte) 18);
+        usi.saveUser("Petr", "Petrov", (byte) 21);
+        usi.saveUser("Sveta", "Svetikova", (byte) 45);
+        usi.saveUser("Rodion", "Rodionov", (byte) 60);
+        usi.getAllUsers();
+        usi.cleanUsersTable();
+        usi.dropUsersTable();
+*/
+
+    }
+}
---
+++ PP_1_1_3-5_JDBC_Hibernate/src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
@@ -0,0 +1,44 @@
+package jm.task.core.jdbc.service;
+
+import jm.task.core.jdbc.dao.UserDao;
+import jm.task.core.jdbc.dao.UserDaoHibernateImpl;
+import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
+import jm.task.core.jdbc.model.User;
+
+import java.util.List;
+
+public class UserServiceImpl implements UserService {
+
+    UserDao dao = new UserDaoHibernateImpl();
+
+    public void createUsersTable() {
+        dao.createUsersTable();
+
+    }
+
+    public void dropUsersTable() {
+        dao.dropUsersTable();
+
+    }
+
+    public void saveUser(String name, String lastName, byte age) {
+        dao.saveUser(name, lastName, age);
+
+    }
+
+    public void removeUserById(long id) {
+        dao.removeUserById(id);
+
+    }
+
+    public List<User> getAllUsers() {
+        List<User> users = dao.getAllUsers();
+        System.out.println(users);
+        return users;
+    }
+
+    public void cleanUsersTable() {
+        dao.cleanUsersTable();
+
+    }
+}
